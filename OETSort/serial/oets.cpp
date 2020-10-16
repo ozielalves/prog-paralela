905dd517dbcf5366064ef9f7287e962ffa6a1686
Oziel Alves <ozielalves@ufrn.edu.br>
2020-10-16 17:37:32 -0300

    OES (Serial) Ready

--- OETSort/serial/oets.cpp
+++ OETSort/serial/oets.cpp
@@ -1,15 +1,80 @@
+#include<bits/stdc++.h>
+#include <sys/time.h> 
+using namespace std; 
+  
+// Ordena a Lista usando Odd and Even Sort 
+void oddEvenSort(int *list, int n) 
+{ 
+    bool isSorted = false; // Flag que indica se a lista está ordenada
+  
+    while (!isSorted) 
+    { 
+        isSorted = true; 
+  
+        // Bubble sort nos elementos começando com index ímpar (Odd)
+        for (size_t i = 1; i <= n-2; i = i+2) 
+        { 
+            if (list[i] > list[i+1]) 
+             { 
+                swap(list[i], list[i+1]); 
+                isSorted = false; 
+              } 
+        } 
+  
+        // Bubble sort nos elementos começando com index par (Even)
+        for (size_t i = 0; i<=n-2; i=i+2) 
+        { 
+            if (list[i] > list[i+1]) 
+            { 
+                swap(list[i], list[i+1]); 
+                isSorted = false; 
+            } 
+        } 
+    } 
+  
+    return; 
+} 
+
+void genList(int *list, int tamanho) {
+
+    srandom(42);
+    for (size_t i = 0; i < tamanho; i++)
+    {   
+        list[i] = random() % 100 ;
+    }
+} 
+  
+// Imprime uma lista de tamanho n
+void printList(int *list, int tamanho) 
+{ 
+    for (size_t i = 0; i < tamanho; i++)
+    {
+       cout << list[i] << " "; 
+    }
+   cout << "\n"; 
+} 
+  
 int main(int argc, char *argv[])
 {
     struct timeval start, stop;
     gettimeofday(&start, 0);
 
-    /* double xa = 0.;
-    double xb = 30.;
+    int n = atoi(argv[1]); // Tamanho da lista
+    int *list = (int*) malloc(n * sizeof(int)); // Aloca o tamanho da lista
 
-    double area_total = trapezioIntegral(xa, xb, atoll(argv[1])); */
+    // Gera lista
+    genList(list, n);
+    
+    // Printa a lista desordenada
+    printList(list, n); 
+    
+    oddEvenSort(list, n); 
+    
+    // Printa a lista ordenada
+    printList(list, n); 
 
     gettimeofday(&stop, 0);
-    FILE *fp;
+    /* FILE *fp;
     char outputFilename[] = "./trapezio/tempo_trapezio.txt";
 
     fp = fopen(outputFilename, "a");
@@ -19,9 +84,11 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    fprintf(fp, "\tTempo: %1.2e \tResultado: %f\n", ((double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec)), area_total);
+    fprintf(fp, "\tTempo: %1.2e \tLista ordenada: %f\n", ((double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec)), area_total);
 
-    fclose(fp);
+    fclose(fp); */
+
+    free(list);
 
     return 0;
 }
